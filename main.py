b8652d3786f05956b19ce6cbb5385d1838dd0c65
Payton Cavanagh <paytoncavanagh34@gmail.com>
2022-07-11 17:59:46 -0700

    Trial one of FamilyDoctorScraper

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from ClientThreads.ClientThreads import *
 from pathvalidate import sanitize_filename #not native
 
 # going to add family doctor thread once I'm done
-THREADS = [FamilyDoctorThread]  #, MedlineThread,WebMDThread,DrugsComThread, MayoClinicThread, CDCThread, 
+THREADS = [NHSScottishThread]  #, MedlineThread,WebMDThread,DrugsComThread, MayoClinicThread, CDCThread, 
 
 def main():
     # Runs every thread at the same time. Might want to wait a bit to run them
